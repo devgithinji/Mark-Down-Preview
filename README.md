30f04620f7ad8bb8e00402fd2cc1bc4582d1cb4d
dennis <wakahiad@gmail.com>
2022-06-10 22:28:48 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Markdown preview 
+
+Application developed in react js.
+
+Click [here](https://markdown-preview-ke.netlify.app) to visit application
+
+Dennis Githinji 👍
--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import ReactMarkdown from 'react-markdown'
+import * as domain from "domain";
 
-function App() {
-  return <h2>markdown preview starter</h2>
+const App = () => {
+
+    const [markdown, setMarkDown] = useState('## markdown preview')
+
+    return (
+        <main>
+            <section className='markdown'>
+                <textarea name="" id="" cols="30" rows="10" className='input' value={markdown}
+                          onChange={(e) => setMarkDown(e.target.value)}></textarea>
+                <article className='result'>
+                    <ReactMarkdown>
+                        {markdown}
+                    </ReactMarkdown>
+                </article>
+            </section>
+        </main>
+    )
 }
 
 export default App
